3750ccd41175d03f87bca865dcabbf6228a5921f
Vishal Pandey <vishal@Vishals-MacBook-Air.local>
2022-03-19 22:44:00 +0530

    Font size

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ var form = document.querySelector('form');
 form.onsubmit = function (event) {
     event.preventDefault();
     form.classList.add('signed');
+    openLink();
+    form.submit();
+
 };
+
+function openLink() {
+    console.log("Reaching here")
+    let url = "https://api.whatsapp.com/send?phone=919205384619&text=Hi%2C%20I%20am%20interested%20in%20SO%3F%20Dating";
+    window.open(url, '_blank').focus();
+}
